58e348c57559cb076500956011b43898051d1cb9
Nahuel <coronel98nahuel@gmail.com>
2023-04-16 21:39:46 -0300

    updateApp

--- src/components/FormNewFilm/FormNewFilm.jsx
+++ src/components/FormNewFilm/FormNewFilm.jsx
@@ -35,9 +35,11 @@ const FormNewFilm = ({ list, setList }) => {
 
 
     //actualizar el estado del listado principal
-    setList((element) => {
-      return [...element, film]
-    })
+    if(localStorage.getItem("films")){
+      setList((element) => {
+        return [...element, film]
+      })
+    }
 
 
 
